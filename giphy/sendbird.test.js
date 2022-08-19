0f1555510b72a278200c410fcc634ef78eb1f502
James Robertson <jamesrobertson@James.Robertson's-MacBook-Pro>
2022-08-19 13:38:57 +0100

    update giphy port

--- giphy/sendbird.test.js
+++ giphy/sendbird.test.js
@@ -1,7 +1,7 @@
 const Sendbird = require('./sendbird');
 const nockBack = require('nock').back
 nockBack.fixtures = __dirname + '/fixtures';
-nockBack.setMode('update');
+nockBack.setMode('dryrun');
 
 describe('sendbird', () => {
     it('send user message with markdown data to channel', async () => {
@@ -13,17 +13,6 @@ describe('sendbird', () => {
 
         const sendbird = new Sendbird();
         await sendbird.sendUserMessage(mockAppData, mockUserId, mockChannelUrl);
-        nockDone();
-
-    });
-
-    it('send bot message with markdown data to channel', async () => {
-        const { nockDone, context } = await nockBack('send-bot-message.json');
-
-        const mockChannelUrl = "sendbird_group_channel_185112538_8ada9ca637b49b7b793f89f6010c89a4aaa4abc7";
-        const mockAppData = "**this is some bold text**";
-        const sendbird = new Sendbird();
-        await sendbird.sendBotMessage(mockAppData, mockChannelUrl);
         nockDone();
 
     });
