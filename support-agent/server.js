f3952259d91f0ac09bfc5db32f704f2eeec22fed
James Robertson <jamesrobertson@James.Robertson's-MacBook-Pro>
2022-10-04 10:36:54 +0100

    switch agent port

--- support-agent/server.js
+++ support-agent/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PORT = process.env.PORT || 8289;
+const PORT = process.env.PORT || 8291;
 
 app.listen(PORT, () => {
     console.log('chat app now listening on port: ', PORT);
